ba4deac1e1758256f83902a1798901f1575d7a72
hieoamr <iamomar1st@gmail.com>
2024-03-28 16:36:07 +0200

    fixed bugs

--- script.js
+++ script.js
@@ -8,11 +8,17 @@ for (var i = 4; i < skills.length; i++) {
 
 // To toggle the skills section visibility
 document.getElementById('toggle-skills').addEventListener('click', function() {
-  for (var i = 0; i < skills.length; i++) {
+  for (var i = 4; i < skills.length; i++) {
     if (skills[i].classList.contains('hide')) {
       skills[i].classList.remove('hide');
     } else {
       skills[i].classList.add('hide');
     }
   }
+//   set the text of the button to hide
+    if (this.innerText === 'View all') {
+        this.innerText = 'View less';
+    } else {
+        this.innerText = 'View all';
+    }
 });
